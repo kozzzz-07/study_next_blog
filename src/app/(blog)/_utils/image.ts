fb55329556996b2e655086c1c98699284e16d5b2
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2025-07-24 16:49:04 +0900

    fix date now

--- src/app/(blog)/_utils/image.ts
+++ src/app/(blog)/_utils/image.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export async function saveImage(file: File): Promise<string | null> {
   const buffer = Buffer.from(await file.arrayBuffer());
-  const fileName = `${Date.now}_${file.name}`;
+  const fileName = `${Date.now()}_${file.name}`;
   const uploadDir = path.join(process.cwd(), "public/images");
   try {
     const filePath = path.join(uploadDir, fileName);
